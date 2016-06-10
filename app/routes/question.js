0e707e856d6ccd97f057fead8559e59676186c0e
Catherine Karra <catherine.karra@gmail.com>
2016-06-10 16:20:29 -0700

    adds a delete button below question details on each questions details page

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+  actions: {
+    delete(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('destroyRental', question);
+      }
+    }
+  }
 });
