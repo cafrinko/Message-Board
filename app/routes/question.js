bd2ec31ea3367bf49ea43f3700d51546ef512cd5
Catherine Karra <catherine.karra@gmail.com>
2016-06-11 12:58:42 -0700

    updates questions

--- app/routes/question.js
+++ app/routes/question.js
@@ -5,6 +5,14 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+    },
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
