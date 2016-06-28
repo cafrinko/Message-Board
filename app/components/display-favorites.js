79d739c1c2aca71958b8e4e1b5f4256e0ed4ec67
Catherine Karra <catherine.karra@gmail.com>
2016-06-27 23:28:20 -0700

    displays favorite questions on favs page via the display favorites component

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  favoriteQuestions: Ember.inject.service(),
-  actions: {
-    add() {
-      this.get('favoriteQuestions').add(this.get('model'));
-    }
-  }
+  favoriteQuestions: Ember.inject.service()
+  // actions: {
+  //   addToFavoriteQuestions(question) {
+  //     this.get('favoriteQuestions').add(question);
+  //   }
+  // }
 });
+// add() {
+//   this.get('favoriteQuestions').add(this.get('model'));
+// }
